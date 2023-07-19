9eea43fc566f0ba208af9ce3c06a9ec017168057
Doriav <doriav20@gmail.com>
2023-07-20 02:02:19 +0300

    print courses

--- grades_manager.py
+++ grades_manager.py
@@ -1,6 +1,7 @@
 import json
 from pathlib import Path
 
+from configuration import Configuration
 from course import Course
 
 
@@ -17,3 +18,13 @@ def load_courses_from_file(grades_file_path: Path) -> list[Course]:
     with open(grades_file_path) as grades_file:
         grades = json.load(grades_file)
         return [Course.from_dict(grade) for grade in grades]
+
+
+def print_courses_by_configuration(courses: list[Course], config: Configuration) -> None:
+    print(f'{"Name":<{config.name_length}}' f'{"Grade":<{config.grade_length}}' f'{"Points":<{config.points_length}}')
+    for course in courses:
+        print(
+            f'{course.name:<{config.name_length}} '
+            f'{course.grade:>{config.grade_length}} '
+            f'{course.points:>{config.points_length}}'
+        )
