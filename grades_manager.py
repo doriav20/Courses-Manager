eb2fbfe53e769494ec68696dec706229272873ec
Doriav <doriav20@gmail.com>
2023-07-20 02:01:59 +0300

    Load courses from file

--- grades_manager.py
+++ grades_manager.py
@@ -1,3 +1,6 @@
+import json
+from pathlib import Path
+
 from course import Course
 
 
@@ -8,3 +11,9 @@ def get_total_points(courses: list[Course]) -> float:
 def get_average_grade(courses: list[Course]) -> float:
     total_points = get_total_points(courses)
     return sum((course.grade * course.points) for course in courses) / total_points
+
+
+def load_courses_from_file(grades_file_path: Path) -> list[Course]:
+    with open(grades_file_path) as grades_file:
+        grades = json.load(grades_file)
+        return [Course.from_dict(grade) for grade in grades]
