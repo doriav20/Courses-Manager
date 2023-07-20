bcb1cd603afc8bcc28e2061fe5ea1c11f0e6dc8c
Doriav <doriav20@gmail.com>
2023-07-20 03:31:41 +0300

    Filter out hidden courses (aka with grade 0)

--- courses_manager.py
+++ courses_manager.py
@@ -6,12 +6,14 @@ from course import Course
 
 
 def get_total_points(courses: list[Course]) -> float:
-    return sum(course.points for course in courses)
+    filtered_courses = [course for course in courses if course.grade > 0]
+    return sum(course.points for course in filtered_courses)
 
 
 def get_gpa(courses: list[Course]) -> float:
-    total_points = get_total_points(courses)
-    return sum((course.grade * course.points) for course in courses) / total_points
+    filtered_courses = [course for course in courses if course.grade > 0]
+    total_points = get_total_points(filtered_courses)
+    return sum((course.grade * course.points) for course in filtered_courses) / total_points
 
 
 def load_courses_from_file(courses_file_path: Path) -> list[Course]:
