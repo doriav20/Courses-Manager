b55b87af121bec3f635121ebfb9434812605277e
Doriav <doriav20@gmail.com>
2023-07-20 03:12:27 +0300

    Add press key to continue logic in utils

---
+++ utils.py
@@ -0,0 +1,12 @@
+import msvcrt
+import os
+
+
+def press_any_key_to_continue() -> None:
+    print("Press any key to continue...")
+    if os.getenv('PYCHARM_HOSTED') == '1':
+        input()
+    elif os.name == 'nt':
+        msvcrt.getch()
+    else:
+        input()
