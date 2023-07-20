8933d0b30793767e40dcf3a509b3c1e1b6b1b7c1
Doriav <doriav20@gmail.com>
2023-07-20 04:14:36 +0300

    Get configuration_path as argument

---
+++ arguments_handler.py
@@ -0,0 +1,42 @@
+import sys
+from pathlib import Path
+from typing import Optional
+
+
+def get_configuration_path_from_arguments() -> Optional[Path]:
+    args = sys.argv[1:]
+
+    if args and Path(args[0]).exists():
+        return Path(args[0])
+
+    return None
+
+
+# Get configuration path by selecting file (ends with json) and start from current dir from file explorer
+def get_configuration_path_from_file_explorer() -> Optional[Path]:
+    from tkinter import Tk
+    from tkinter.filedialog import askopenfilename
+
+    Tk().withdraw()
+    filename = askopenfilename(
+        title="Select configuration file",
+        filetypes=[("Configuration JSON files", "*.json")],
+        initialdir=Path.cwd(),
+    )
+
+    if filename:
+        return Path(filename)
+
+    return None
+
+
+def get_configuration_path() -> Optional[Path]:
+    configuration_path = get_configuration_path_from_arguments()
+    if configuration_path:
+        return configuration_path
+
+    configuration_path = get_configuration_path_from_file_explorer()
+    if configuration_path:
+        return configuration_path
+
+    return None
